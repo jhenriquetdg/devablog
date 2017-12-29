0f39d890bce1f9bebe4d479361ee4e0696afbd54
Zé Henrique <jhenriquetdg@gmail.cm>
2017-12-29 16:33:19 -0300

    End of chapter 5.

---
+++ devablog.py
@@ -0,0 +1,12 @@
+from app import app, db
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    context = dict()
+    context['db'] = db;
+    context['Post'] = Post;
+    context['User'] = User;
+
+    return context
