281c57db6fed91b1781ef204db35e8f027592571
Zé Henrique <jhenriquetdg@gmail.cm>
2018-01-01 16:11:47 -0300

    Begin of chapter 12

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask import Flask
 from flask_mail import Mail
 from flask_login import LoginManager
+from flask_moment import Moment
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -22,6 +23,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
